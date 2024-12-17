84bf601d9ae55e8bdb015886965922330fab15cc
sam <aquaticcalf@proton.me>
2024-12-17 10:05:43 +0000

    meow

--- sound.js
+++ sound.js
@@ -2,7 +2,13 @@ const audio_ctx = new (window.AudioContext || window.webkitAudioContext)()
 
 function play_sound(frequency = 440, duration = 1) {
     const oscillator = audio_ctx.createOscillator()
-    oscillator.connect(audio_ctx.destination)
+    const envelope = audio_ctx.createGain()
+    oscillator.connect(envelope)
+    envelope.connect(audio_ctx.destination)
+
+    envelope.gain.setValueAtTime(0, audio_ctx.currentTime)
+    envelope.gain.linearRampToValueAtTime(1, audio_ctx.currentTime + 0.05)
+    envelope.gain.linearRampToValueAtTime(0, audio_ctx.currentTime + duration)
 
     oscillator.frequency.setValueAtTime(frequency, audio_ctx.currentTime)
 
