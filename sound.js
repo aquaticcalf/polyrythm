e2de19ed126894ad5e35cb739970445fc57b44fc
sam <aquaticcalf@proton.me>
2024-12-17 13:05:07 +0000

    meow

--- sound.js
+++ sound.js
@@ -7,7 +7,7 @@ function play_sound(frequency = 440, duration = 1) {
     envelope.connect(audio_ctx.destination)
 
     envelope.gain.setValueAtTime(0, audio_ctx.currentTime)
-    envelope.gain.linearRampToValueAtTime(1, audio_ctx.currentTime + 0.05)
+    envelope.gain.linearRampToValueAtTime(0.05, audio_ctx.currentTime + 0.05)
     envelope.gain.linearRampToValueAtTime(0, audio_ctx.currentTime + duration)
 
     oscillator.frequency.setValueAtTime(frequency, audio_ctx.currentTime)
