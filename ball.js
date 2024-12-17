d2160db94fbf7770c54e9f755dbc29c25d54c1a5
sam <aquaticcalf@proton.me>
2024-12-17 10:30:49 +0000

    meow

--- ball.js
+++ ball.js
@@ -1,8 +1,9 @@
 class Ball {
-    constructor ( track, radius, speed ) {
+    constructor ( track, radius, speed, sound_frequency ) {
         this.track = track
         this.radius = radius
         this.speed = speed
+        this.sound_frequency = sound_frequency
         this.offset = 0
         this.direction = 1
         this.center = this.track.get_position(this.offset)
@@ -13,7 +14,7 @@ class Ball {
         this.center = this.track.get_position(this.offset)
         if ( this.center.y > this.track.center.y ) {
             this.direction = this.direction * -1
-            play_sound()
+            play_sound(this.sound_frequency)
         }
     }
 
