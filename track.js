4745992975cdb83e1463ca18a0af787d257e8b74
sam <aquaticcalf@proton.me>
2024-12-17 09:37:52 +0000

    meow

---
+++ track.js
@@ -0,0 +1,20 @@
+class Track {
+    constructor ( center, radius ) {
+        this.center  = center
+        this.radius = radius
+    }
+
+    get_position(offset) { // unit circle
+        return {
+            x: this.center.x + Math.cos(offset) * this.radius,
+            y: this.center.y - Math.sin(offset) * this.radius
+        }
+    }
+
+    draw(ctx) {
+        ctx.beginPath()
+        ctx.arc(this.center.x, this.center.y, this.radius, 0, Math.PI * 2)
+        ctx.strokeStyle = "white"
+        ctx.stroke()
+    }
+}
